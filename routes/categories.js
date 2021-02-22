b63523e65c46bb3222996e3f315e4aaee0e2b4fb
kezakimana <kezakimanachrist2@gmail.com>
2021-02-22 19:43:17 +0200

    add bootstrap librarie

---
+++ routes/categories.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/categories",function (req, res, next) {
+ 
+    res.render("categories/index");
+});
+
+module.exports = router;
